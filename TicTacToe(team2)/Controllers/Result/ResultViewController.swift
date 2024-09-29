581983061f5a2963cbd1111482b7c31b86006727
Igor Guryan <sacujo93@yandex.ru>
2024-09-30 01:33:06 +0300

    make scheme for project

---
+++ TicTacToe(team2)/Controllers/Result/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  TicTacToe(team2)
+//
+//  Created by Igor Guryan on 30.09.2024.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
---
+++ TicTacToe(team2)/Controllers/SelectGame/SelectGameViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Untitled.swift
+//  TicTacToe(team2)
+//
+//  Created by Igor Guryan on 30.09.2024.
+//
+
+import UIKit
+
+class SelectGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
