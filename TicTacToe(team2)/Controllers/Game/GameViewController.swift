5e06907cc1c1e859ecf03a5afb192ba44f0f14a7
AnnaMelekh <annasmelekhina@gmail.com>
2024-09-30 12:56:27 +0200

    preview

--- TicTacToe(team2)/Controllers/Game/GameViewController.swift
+++ TicTacToe(team2)/Controllers/Game/GameViewController.swift
@@ -11,8 +11,9 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        view.backgroundColor = .systemRed
     }
 
-
 }
+
+#Preview { GameViewController() }
