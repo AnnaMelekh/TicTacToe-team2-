9271c58363a8f7f1ddab1dfc94e7ae2b1e30c9e6
Boogie-Wo0gie <120246769+Boogie-Wo0gie@users.noreply.github.com>
2024-09-30 23:11:06 +0300

    t
    est

--- TicTacToe(team2)/Controllers/Result/ResultViewController.swift
+++ TicTacToe(team2)/Controllers/Result/ResultViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
-    }
+        
+        
+        view.addSubview(backgroundImageView)
+        
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
 
-
-    
-    
-    func justDoIt() {
-        print("DO IT")
+        
+ 
+        
     }
-    
 }
